3f4568aa8de17cbf4977c48e3f4a5a9aedef0d81
Tutuzxx <kauar1253@gmail.com>
2025-05-26 21:25:52 -0300

    Update desafioExtra2.php

--- phpAula/desafioExtra2.php
+++ phpAula/desafioExtra2.php
@@ -1,7 +1,14 @@
 <?php
+ 
 $lista = [];
-$lista1 = [1,'a',23,56,'ou','i', 'O' ,'s' ,'W',13,9,74,55,'m','z','e','u','ç','l','b',9];
-$usuarioSenha = readline(" Me fale um número 0 a 10 como criterio para a senha:");
-if($usuarioSenha == 1 ){
-    rand(1);
+$lista1 = [1, 'a', 23, 56, 'ou', 'i', 'O', 's', 'W', 13, 9, 74, 55, 'm', 'z', 'e', 'u', 'ç', 'l', 'b', 9];
+ 
+$usuarioSenha = (int) readline("Me fale um número de 1 a 10 para a senha: ");
+ 
+for ($i = 0; $i < $usuarioSenha; $i++) {
+    $itemAleatorio = $lista1[rand(0, count($lista1) - 1)];
+    $lista[] = $itemAleatorio;
+    echo $itemAleatorio;
 }
+ 
+echo "\n";
